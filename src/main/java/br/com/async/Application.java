1f9cd5cf8d3493dc5ca6b888ab8506aef7a8aeb2
Luis Lucana <luislucana@gmail.com>
2020-02-06 10:31:36 -0300

    Criação dos arquivos iniciais.

--- src/main/java/br/com/async/Application.java
+++ src/main/java/br/com/async/Application.java
@@ -1,23 +1,25 @@
 package br.com.async;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * https://howtodoinjava.com/spring-restful/multipart-upload-download-example/
  */
-//@EnableAutoConfiguration
 //@EnableConfigurationProperties({
         //FileStorageProperties.class
 //})
+@EnableAutoConfiguration
 @ComponentScan("br.com.async.*")
 @EntityScan("br.com.async.model")
 @EnableJpaRepositories("br.com.async.repository")
+@EnableTransactionManagement
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
--- src/main/java/br/com/async/web/exception/CustomExceptionHandler.java
+++ src/main/java/br/com/async/web/exception/CustomExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.async.web.exception;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -18,6 +20,16 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler
 {
 	private String INCORRECT_REQUEST = "INCORRECT_REQUEST";
 	private String BAD_REQUEST = "BAD_REQUEST";
+
+	@ExceptionHandler(FileNotFoundException.class)
+	public final ResponseEntity<ErrorResponse> fileNotFoundException
+			(FileNotFoundException ex, WebRequest request)
+	{
+		List<String> details = new ArrayList<>();
+		details.add(ex.getLocalizedMessage());
+		ErrorResponse error = new ErrorResponse(INCORRECT_REQUEST, details);
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
 	
 	@ExceptionHandler(RecordNotFoundException.class)
 	public final ResponseEntity<ErrorResponse> handleUserNotFoundException
